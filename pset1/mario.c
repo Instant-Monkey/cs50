cc74b1faee16fdb24f3f5b59151de40a5339008f
Sylvain Laugier <sylvain.laugier.net@gmail.com>
2017-08-27 08:49:36 +0000

    initial commit

---
+++ pset1/mario.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) {
+
+    int repet = -1;
+    while (repet <0 || repet >23){
+        printf("height:");
+         repet = get_int();
+    }
+    for (int i = 1 ; i <= repet; i++) {
+        //iterate through each row
+        //left side
+        for (int j = repet; j > 0; j--){
+            if (j <= i){
+                printf("#");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("  ");
+        //right side
+         for (int j = 1; j <= repet; j++){
+            if (j <= i){
+                printf("#");
+            }
+        }
+        printf("\n");
+    }
+}
